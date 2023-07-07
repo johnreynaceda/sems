966f34efe52263e29b7bf3f33b86ea20d6cda3dc
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-07-07 15:21:37 +0800

    SALES, EXPENSES REPORT

--- app/Http/Livewire/Report.php
+++ app/Http/Livewire/Report.php
@@ -8,13 +8,19 @@ use App\Models\ExpenseTransaction;
 
 class Report extends Component
 {
-    public $report;
+    public $report = 1;
     public $date_from, $date_to;
     public function render()
     {
         return view('livewire.report', [
             'reports' => $this->generateReport(),
         ]);
+    }
+
+    public function updatedReport()
+    {
+        $this->date_from = null;
+        $this->date_to   = null;
     }
 
     public function generateReport()
@@ -26,7 +32,11 @@ class Report extends Component
                 $query->whereDate('dot', '<=', $this->date_to);
             })->get()->take(10);
         } else {
-            return ExpenseTransaction::get();
+            return ExpenseTransaction::when($this->date_from, function ($query) {
+                $query->whereDate('dot', '>=', $this->date_from);
+            })->when($this->date_to, function ($query) {
+                $query->whereDate('dot', '<=', $this->date_to);
+            })->get()->take(10);
         }
     }
 }
